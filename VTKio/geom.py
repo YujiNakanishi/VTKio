1a550d1150be26f9b82ef81b7eea23016f192370
NAKANISHI\sygha <syghabc@gmail.com>
2024-06-25 13:06:19 +0900

    unstructured grid

--- VTKio/geom.py
+++ VTKio/geom.py
@@ -79,3 +79,46 @@ class structured_points(version2):
             file.write("DIMENSIONS {0} {1} {2}\n".format(num_grids[0], num_grids[1], num_grids[2]))
             file.write("ORIGIN {0} {1} {2}\n".format(origin[0], origin[1], origin[2]))
             file.write("SPACING {0} {1} {2}\n".format(spacing[0], spacing[1], spacing[2]))
+
+
+class unstructured_grid(version2):
+    geom_type = "UNSTRUCTURED_GRID"
+    def __init__(self, points : np.ndarray, cells : tuple, point_data = [], cell_data = []):
+        super().__init__(point_data, cell_data)
+        self.points = points
+        self.cells = cells
+    
+    @property
+    def dim(self) -> int: return self.points.shape[-1]
+    @property
+    def num_points(self) -> int: return len(self.points)
+    @property
+    def num_cells(self) -> int: return len(self.cells)
+
+    def write_dataset(self, filename : str):
+        points = np.concatenate((self.points, np.zeros((self.num_points, 1))), axis = 1) if self.dim == 2 else self.points
+        with open(filename, "a") as file:
+            file.write("POINTS {} float\n".format(self.num_points))
+            for point in points:
+                file.write(self.np2str(point))
+            
+            size = 0
+            for cell in self.cells:
+                size += 1 + len(cell["indice"])
+            file.write("CELLS {0} {1}\n".format(self.num_cells, size))
+            for cell in self.cells:
+                s = str(len(cell["indice"])) + " " + self.np2str(cell["indice"])
+                file.write(s)
+            
+            file.write("CELL_TYPES {}\n".format(self.num_cells))
+            for cell in self.cells:
+                file.write("{}\n".format(cell["type"]))
+
+
+class point_cloud(unstructured_grid):
+    def __init__(self, points : np.ndarray, point_data = []):
+        cells = tuple(({"type" : 1, "indice" : np.array([i])} for i in range(len(points))))
+        super().__init__(points, cells, point_data, []) #cell_dataは未定義とする
+    
+    def add_celldata(self, name : str, values : np.ndarray):
+        raise Exception("point cloud can't define cell data")
